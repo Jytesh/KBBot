20f1c18478c930692ea32f148369e0fa7c5f9d4d
Jytesh <44925963+Jytesh@users.noreply.github.com>
2020-02-19 16:25:38 +0530

    Game Info YEEEEEEEEEEEEEEEEEEEEEEEEEEET

--- commands/lfg.js
+++ commands/lfg.js
@@ -3,7 +3,7 @@ const config = require("../config.json")
 const utils = require("../utils")
 
 const {NA,EU,OCE,AS} = require("../utils.js").channels
-module.exports.run = (client,message)=>{
+module.exports.run = async(client,message)=>{
         var args = message.content.substring(1).split(' ');
     
         if(args.length < 2) {
@@ -18,7 +18,7 @@ module.exports.run = (client,message)=>{
                 if(link == "NA" || link == "OCE" || link == "AS" || link == "EU"){
                     channel = getChannel(link)
                     if(channel === false){
-                        utils.ErrorMsg(message,"Invalid Region")
+                        utils.ErrorMsg(message,"Invalid Region\nError Code: 101")
                     }else{
                     const embedLfg = new Discord.RichEmbed()
                     .setTitle(message.author.username + ' is looking for people.')
@@ -31,30 +31,38 @@ module.exports.run = (client,message)=>{
                 }
                 channel.send(embedLfg);
                 }}else{
-                utils.ErrorMsg(message,"Invalid link/region")
+                    utils.ErrorMsg(message,"Invalid Region/Link\nError Code: 100")
                 }
             }else {
                 channel = getChannel(link,true)
                 if(channel === false){
-                    utils.ErrorMsg(message,"Invalid Region/Link")
+                    utils.ErrorMsg(message,"Invalid Region/Link\nError Code: 100")
                     return;
                 }else{
+                    getLinkInfo(link).then(game =>{
+                    
                 const embedLfg = new Discord.RichEmbed()
                     .setTitle(random(message.author.username))
-                    .setAuthor(message.author.username, message.author.displayAvatarURL)
-                    .addField('Link:', link, false)
+                    .setAuthor(message.author.tag, message.author.displayAvatarURL)
+                    .addField('Link: ', link,)
+                    .addField('Map: ',game.map)
+                    .addField("Players: ",game.players)
+                    .addField("Custom?: ",game.custom)
                     .setTimestamp()
                     .setFooter('KrunkerLFG');
                 if(args.length >= 1) {
-                    eb.setDescription(args.join(' '));
+                    embedLfg.setDescription(args.join(' '));
                 }
                 channel.send(embedLfg);
+            }).catch(e =>{
+                    utils.ErrorMsg(message,"Invalid server link provided\nError Code : 404")
             }
+            )}
             }
         
         }
     }
-    function random(tempUser) {
+function random(tempUser) {
         var num = Math.floor(Math.random()*8);
         let string
         switch(num) {
@@ -93,7 +101,6 @@ function getChannel(id,val){
         region = link.split(":")[0]
         id = region
     }
-    console.log(id)
     if(id == "NA" || id == "NY" || id == "MIA" || id == "SV"){
         id = NA
     }else if(id == "EU" || id == "FRA"){
@@ -105,6 +112,30 @@ function getChannel(id,val){
     }else return false
     return client.channels.get(id)
 }
+function getLinkInfo(link){
+    return new Promise((resolve, reject) =>{
+    link = link.split("=")[1]
+    if(!link) return false;
+    else{
+        const request = require("request")
+        request({uri:`https://matchmaker.krunker.io/game-info?game=${link}`}, (err, res, json) =>{
+            json = JSON.parse(json)
+        if(err || json.error){
+            return reject("404",err)
+        }
+        else{
+            
+            
+        const game = {
+            region : json[0].split(":")[0],
+            players: `${json[2]}/${json[3]}`,
+            map: json[4].i.split("_")[1],
+            custom: json[4].cs
+        }
+        return resolve(game)
+        }})
+    }})
+}
 module.exports.config = {
     name : "lfg",
     aliases : ["looking", "lf", "lfm"]
@@ -114,26 +145,4 @@ module.exports.help = {
     User : 1, //Who this command can be used by, 1 for Everyone 2 for Restricted Roles 3 for Moderators and 4 for Admins 5 for Server Owner
     Roles : ["678830128968499220"], //Guest role, the only role which can use this command
     description : `To look for groups` //Description to come when you use config.prefix help <command name>
-}
-
-function randomMessage(tempUser) {
-    var num = Math.floor(Math.random*8);
-    switch(num) {
-        case 0:
-            return tempUser + ' is here to stomp some Guests.';
-        case 1:
-            return tempUser + ' is here to flex their skins.';
-        case 2:
-            return 'Omg ' + tempUser + ' Is UsIng hAcks. rEpOrtEd.';
-        case 3:
-            return 'Krunker? Never heard of it.';
-        case 4:
-            return 'Knowledge is knowing a tomato is a fruit; Wisdom is not putting' + tempUser + 'in a fruit salad.';
-        case 5:
-            return 'If being ' + tempUser + ' is a crime, then arrest me.';
-        case 6:
-            return 'A bus station is where a bus stops. A train station is where a train stops. On ' + tempUser + '\'s desk, I have a work station..';
-        case 7:
-            return 'Politicians and diapers have one thing in common. ' + tempUser + ' should change them both regularly, and for the same reason.';
-    }
 }
