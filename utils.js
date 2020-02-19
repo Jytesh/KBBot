2250ed07eee56d29e8812d52539881f5d1f2b9c0
JJ-G4M3R <josephzxhu@gmail.com>
2020-02-19 12:16:17 -0800

    reworked lfg

--- utils.js
+++ utils.js
@@ -5,6 +5,14 @@ channels = {
     OCE : "678830581512929331",
     AS : "678830592946602002"
 }
+gamemodes = {
+    ffa: 0x66de5b,
+    tdm: 0x45e6e3,
+    ctf: 0xd13c2e,
+    point: 0xba4911,
+    party: 0x3611ba,
+    other: 0xa8114b
+}
 module.exports = {
     channels,
     ErrorMsg : function(m,text){
@@ -15,9 +23,9 @@ module.exports = {
         .setFooter("Krunker LFG |ID :"+m.author.id)
         .setTimestamp()
 
-    m.channel.send(embed).then(mess=>
-        mess.delete(10000)
-    )
-
+        m.channel.send(embed).then(mess=>
+            mess.delete(10000)
+        )
     }
 }
+var autodel = false;
