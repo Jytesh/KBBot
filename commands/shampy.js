347fc8ee087d52ef5ee48581ab2547dada0e7fb5
JJ-G4M3R <josephzxhu@gmail.com>
2020-03-01 14:55:29 -0800

    Minor edit

--- commands/shampy.js
+++ commands/shampy.js
@@ -14,7 +14,7 @@ module.exports.config = {
 }
 
 module.exports.help = {
-    usage : `\`${config.prefix}info\``, //Example usage of command
+    usage : `\`${config.prefix}shampy\``, //Example usage of command
     User : 0, //Who this command can be used by, 1 for Everyone 2 for Restricted Roles 3 for Moderators and 4 for Admins 5 for Server Owner
-    description : `info lists all the info about the bot.` //Description to come when you use config.prefix help <command name>
+    description : `Shhh you found the hidden command` //Description to come when you use config.prefix help <command name>
 }
