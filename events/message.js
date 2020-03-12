e14a1ca53d2e7bfe8cf84db2921a5bbc763b0d98
JJ-G4M3R <josephzxhu@gmail.com>
2020-03-11 22:42:08 -0700

    cleaning

--- events/message.js
+++ events/message.js
@@ -2,19 +2,20 @@ const Discord = require("discord.js");
 const config = require("../config.json");
 const db = require("../json.db")
 module.exports = async (client, message) => {
-    prefix = await db.prefix(message.guild.id)
-    if (!message.guild || message.author.bot) return; // This will prevent bots from using the bot (and will also disable DMs). Lovely!
-    const args = message.content.split(/\s+/g); // This will return the message content and split the prefix.
-    const command = message.content.startsWith(prefix) && args.shift().slice(prefix.length).toLowerCase(); // This is the name of the command itself.
-    
-    if(command){
-        let commandfile = client.commands.get(command) || client.commands.get(client.aliases.get(command)); // This will look for the command's file by searching it in names and aliases.
-        
-        if(commandfile) commandfile.run(client, message, args); // And if it finds the command, it will run it.
-    }
-    if(message.mentions.users.first() == client.user){
-        console.log(message.cleanContent)
-        if(message.cleanContent.startsWith("@"+client.user.username)) require("../utils").embed(message,`\`${prefix}\` is my prefix, try \`${prefix}help\`.`)
-        
-    }
+    client.setTimeout(() => {
+        if(!message.deleted){
+            prefix = await db.prefix(message.guild.id)
+            if (!message.guild || message.author.bot) return; // This will prevent bots from using the bot (and will also disable DMs). Lovely!
+                const args = message.content.split(/\s+/g); // This will return the message content and split the prefix.
+                const command = message.content.startsWith(prefix) && args.shift().slice(prefix.length).toLowerCase(); // This is the name of the command itself.
+            if(command){
+                let commandfile = client.commands.get(command) || client.commands.get(client.aliases.get(command)); // This will look for the command's file by searching it in names and aliases.
+                if(commandfile) commandfile.run(client, message, args); // And if it finds the command, it will run it.
+            }
+            if(message.mentions.users.first() == client.user){
+                console.log(message.cleanContent)
+                if(message.cleanContent.startsWith("@"+client.user.username)) require("../utils").embed(message,`\`${prefix}\` is my prefix, try \`${prefix}help\`.`) 
+            }
+        }
+    }, 1*1000);
 };
