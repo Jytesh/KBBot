bd107b09c9ecd5d913062e6a411ff5c5001970f4
JJ-G4M3R <josephzxhu@gmail.com>
2020-03-15 00:04:56 -0700

    Status change to version

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,8 @@
 const {MessageEmbed} = require("discord.js")
 const client = require("../app.js").client
+const config = require("../config.json")
 
 module.exports = (client)=>{ 
     console.log("[Krunker Bunker Bot] ready to roll!")
-    client.user.setActivity("krunker.io",{type: "PLAYING"})
+    client.user.setActivity(`v${config.version}`,{type: "RUNNING"})
 }
