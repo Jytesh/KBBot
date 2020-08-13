a091d6236134a42b5a147f9ed75499a0596ae786
JJ-G4M3R <josephzxhu@gmail.com>
2020-08-12 23:55:38 -0700

    Updated stuff

--- app.js
+++ app.js
@@ -4,7 +4,13 @@ const config = require("./config.json");
 const client = new Discord.Client();
 const fs = require("fs");
 const path = require("path");
-const env = require("dotenv")
+const env = require("dotenv");
+const db = require("./mongo.js");
+
+async function init() {
+    await db.init().catch(console.error);
+}
+init();
 
 //Login
 env.config()
@@ -12,36 +18,36 @@ client.login(process.env.TOKEN)
 
 //Loading Events from /events directory
 fs.readdir("./events/", (err, files) => { //Getting all files from directory
-  if (err) return console.error(err);
-  let jsfile = files.filter(f => f.split(".").pop() === "js")
-  if(jsfile.length <= 0){
+    if (err) return console.error(err);
+    let jsfile = files.filter(f => f.split(".").pop() === "js")
+    if (jsfile.length <= 0) {
         return console.log("[KB Bot] There aren't any events!");
     }
-  jsfile.forEach(file => { //For each file, ddo this.
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    client.on(eventName, event.bind(null, client)); //Name of the file is the name of the event. https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e
-    delete require.cache[require.resolve(`./events/${file}`)];
-  });
+    jsfile.forEach(file => { //For each file, ddo this.
+        const event = require(`./events/${file}`);
+        let eventName = file.split(".")[0];
+        client.on(eventName, event.bind(null, client)); //Name of the file is the name of the event. https://discord.js.org/#/docs/main/stable/class/Client?scrollTo=e
+        delete require.cache[require.resolve(`./events/${file}`)];
+    });
 });
 
 //Loading commands from /commands directory, to client
 client.commands = new Discord.Collection(); //Discord's Collection Class, extends map.
 client.aliases = new Discord.Collection();
 
-fs.readdir("./commands/", (err, files) => { 
-    if(err) console.error(err);
+fs.readdir("./commands/", (err, files) => {
+    if (err) console.error(err);
     let jsfile = files.filter(f => f.split(".").pop() === "js");
-    if(jsfile.length <= 0){
+    if (jsfile.length <= 0) {
         return console.log("[KB Bot] There aren't any commands!"); //JJ has fucked up
     }
     jsfile.forEach((f, i) => {
-      let pull = require(`./commands/${f}`)
-      
-      client.commands.set(pull.config.name, pull);
-      pull.config.aliases.forEach(alias => {
-        client.aliases.set(alias, pull.config.name); //Store this data in our client(Only in cache, this data doesn't goto discord)
-      })
+        let pull = require(`./commands/${f}`)
+
+        client.commands.set(pull.config.name, pull);
+        pull.config.aliases.forEach(alias => {
+            client.aliases.set(alias, pull.config.name); //Store this data in our client(Only in cache, this data doesn't goto discord)
+        })
     });
 });
 
--- events/message.js
+++ events/message.js
@@ -1,8 +1,6 @@
 const Discord = require("discord.js");
 const config = require("../config.json");
 
-var lastTime = new Date().getTime();
-
 module.exports = async(client, message) => {
     client.setTimeout(async() => {
         if (!message.deleted) {
@@ -11,7 +9,7 @@ module.exports = async(client, message) => {
             if (!message.guild) return; // This will prevent the bot from responding to DMs. Lovely!
 
             if (message.channel.id == '688434522072809500') { //#looking-for-game
-                // client.commands.get('lfg').run(client, message)
+                //client.commands.get('lfg').run(client, message)
             } else if (message.channel.id == '687539638168059956' || message.channel.id == '679429025445445643') { //#bunker-bot-commands and #dev
                 if (message.content.indexOf(`${config.prefix}info`) == 0) {
                     client.commands.get('info').run(client, message)
@@ -20,13 +18,14 @@ module.exports = async(client, message) => {
                 }
             } else if (message.channel.id == '710454866002313248' && !message.member.roles.cache.has('692870902005629041') && !message.member.roles.cache.has('448207247215165451')) { //#trading-board
                 if (message.content.indexOf('https://krunker.io/social.html?p=profile&q=') < 0 && message.attachments.size == 0) {
-                    message.channel.send(`<@${message.author.id}> All trades require an inventory screenshot with the username visible, or a link to your account. \n *This is an automated message.*`);
+                    message.channel.send(`<@${message.author.id}> Your message was deleted for breaking the following rule(s): \n > All trades require an inventory screenshot with the username visible, or a link to your account. \n *This is an automated message.*`);
                     message.delete();
                 }
             } else if (message.channel.id == '604386199976673291') { //#market-chat
-                if (message.content.indexOf('trad') >= 0 && new Date().getTime() - lastTime > 120000) {
-                    message.channel.send('Trade/Trading? Make sure you are in the right channel. \n - <#604386199976673291> is for trade advice \n - <#710454866002313248> is for trade listings. \n *This is an automated message.*')
-                    lastTime = new Date().getTime();
+                if (message.content.indexOf(`${config.prefix}trade`) == 0 && (message.member.roles.cache.has('448207247215165451') || message.author.id == '425441636449910807' || message.author.id == '363756486100385801')) {
+                    const str = message.content.split(' ').length == 2 ? `<@${message.content.split(' ')[1]}> ` : '';
+                    message.channel.send(`${str}Trade/Trading? Make sure you are in the right channel. \n - <#604386199976673291> is for trade advice \n - <#710454866002313248> is for trade listings.`)
+                    message.delete();
                 }
             }
         }
