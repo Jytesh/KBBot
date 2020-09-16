7259004673f9f18b873782cb73953cc7c77f989c
JJ-G4M3R <30411592+JJ-G4M3R@users.noreply.github.com>
2020-09-15 19:43:27 -0700

    Update suggestions.js

--- commands/suggestions.js
+++ commands/suggestions.js
@@ -1,1 +1,25 @@
-f
+const { MessageEmbed } = require("discord.js")
+
+module.exports.run = (client, message) => {
+    var args = message.content.split(' ');
+    const sid = args.shift();
+    const suggestion = args.join(' ');
+    const eb = new MessageEmbed()
+        .setColor('YELLOW')
+        .setDescription(`${suggestion} \n\n - Suggested by <@${sid}>`)
+    message.channel.send(eb).then(sentEmbed => {
+        sentEmbed.react("👍");
+        sentEmbed.react("👎");
+    });
+    message.delete();
+}
+
+module.exports.config = {
+    name: 'suggestions',
+    aliases: ['sug'],
+}
+module.exports.help = {
+    usage: '',
+    User: 0,
+    description: ''
+}
