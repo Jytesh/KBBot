f85f2e8bf91a06735831cd87835b6f246db8c11f
JJ-G4M3R <josephzxhu@gmail.com>
2020-10-26 17:00:13 -0700

    Update

--- commands/trading.js
+++ commands/trading.js
@@ -15,7 +15,7 @@ const users = [];
 module.exports.run = (client, message) => {
     var canBypass = false;
     if (!canBypass) roles.forEach(role => { if (message.member.roles.cache.has(role)) canBypass = true; return });
-    if (!canBypass && !message.content.contains('https://krunker.io/social.html?p=profile&q=') && !message.content.contains('https://kr.social/p/') && message.attachments.size == 0) {
+    if (!canBypass && !message.content.indexOf('https://krunker.io/social.html?p=profile&q=') < 0 && !message.content.indexOf('https://kr.social/p/') < 0 && message.attachments.size == 0) {
         message.channel.send(`<@${message.author.id}> Your message was deleted for breaking the following rule(s): \n > All trades require an inventory screenshot with the username visible, or a link to your account. \n *This is an automated message.*`);
         message.delete();
     }
