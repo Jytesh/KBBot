b389c271b5c84a4e89a12df34ea34e47ddd45399
JJ-G4M3R <josephzxhu@gmail.com>
2020-10-29 21:28:42 -0700

    Hacker reports update

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ client.login(process.env.TOKEN);
 //Event Handlers
 client.on('ready', async() => {
     const ts = new Date();
-    console.log(`${ts.getFullYear()}-${ts.getMonth()}-${ts.getDate()}T${ts.getHours()}:${ts.getMinutes()}:${ts.getSeconds()}.${ts.getMilliseconds()}B${config.version} [Krunker Bunker Bot] ready to roll!`);
+    console.log('[Krunker Bunker Bot] ready to roll!');
     client.user.setActivity(`v${config.version}`, { type: "WATCHING" });
 });
 
@@ -38,14 +38,17 @@ client.on('message', async(message) => {
                 case '710454866002313248': // #trading-board
                     client.commands.get('trading').run(client, message);
                     break;
-                case '604386199976673291': //#market-chat
+                case '604386199976673291': // #market-chat
                     client.commands.get('market').run(client, message);
                     break;
-                case '727526422381461514': //#krunker-art
+                case '727526422381461514': // #krunker-art
                     client.commands.get('art').run(client, message);
                     break;
-                case '534664336027680768': //#suggestions
+                case '534664336027680768': // #suggestions
                     client.commands.get('suggestions').run(client, message);
+                    break;
+                case '727452241749082113': // #report-hackers
+                    client.commands.get('reporthackers').run(client, message);
                     break;
             }
 
@@ -60,7 +63,6 @@ client.on('message', async(message) => {
 
 //Loading commands from /commands directory, to client
 client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
 
 fs.readdir("./commands/", (err, files) => {
     if (err) return console.error(err);
@@ -69,7 +71,6 @@ fs.readdir("./commands/", (err, files) => {
     jsfile.forEach((f, i) => {
         const pull = require(`./commands/${f}`)
         client.commands.set(pull.config.name, pull);
-        pull.config.aliases.forEach(alias => { client.aliases.set(alias, pull.config.name) }) //Store this data in our client(Only in cache, this data doesn't goto discord)
     });
 });
 
---
+++ commands/reporthackers.js
@@ -0,0 +1,55 @@
+const logger = require('../logger'),
+    { MessageEmbed } = require("discord.js");
+
+
+const roles = [
+    '692870902005629041', //Trial Mod
+    '448207247215165451', //Mod
+    '448195089471111179', //CM
+    '638129127555072028', //Yendis
+    '448198031041495040', //Dev
+];
+
+const rules = {
+    social: 'Reports must include their **social profile link**. Acceptable urls: \n - https://kr.social/p/ \n - https://krunker.io/social.html?p=profile&q=',
+    video: 'Reports must include a **YouTube** video, a **Streamable** video, a **Loom** video, or a **Twitch** clip when submitting a report. We will only accept video evidence from those platforms. \n - Reports with videos uploaded to any other video sharing platform will be deleted. \n - If your video or clip is deleted and is unavailable in the future, users that you reported may be unflagged.',
+};
+
+module.exports.run = (client, message) => {
+    var canBypass = false;
+    if (!canBypass) roles.forEach(role => { if (message.member.roles.cache.has(role)) canBypass = true; return });
+    if (!canBypass) {
+        let rulesBroken = '';
+        if (!message.content.includes('https://kr.social/p/') && !message.content.includes('https://krunker.io/social.html?p=profile&q=')) rulesBroken += `► ${rules.social} \n`;
+        if (!message.content.includes('https://www.youtube.com/watch?v=') && !message.content.includes('https://youtu.be/') && !message.content.includes('https://streamable.com/') && !message.content.includes('https://www.loom.com/share/') && !message.content.includes('https://clips.twitch.tv/') && !message.content.includes('https://www.twitch.tv/')) rulesBroken += `► ${rules.video}`;
+        if (rulesBroken != '') {
+            message.reply(new MessageEmbed()
+                .setTitle('Please make sure you read the rules about submitting a report')
+                .setColor('ORANGE')
+                .setDescription('Your report has broken the following rule(s): \n' + rulesBroken)
+                .setTimestamp()
+            ).then(msg => { msg.delete({ timeout: 20000 }) }).catch(console.error);
+            logger.messageDeleted(message, 'Hacker Report - Missing required info');
+            message.delete();
+        } else {
+            message.reply(new MessageEmbed()
+                .setTitle('Thank you')
+                .setColor('GREEN')
+                .setDescription('Your report has been submitted for review. Thank you for the report.')
+                .setTimestamp()
+            ).then(msg => { msg.delete({ timeout: 20000 }) }).catch(console.error);
+            client.channels.cache.get('727467397740625951').send(new MessageEmbed()
+                .setAuthor(`${message.author.tag} (${message.author.id})`, message.author.displayAvatarURL())
+                .setColor('BLURPLE')
+                .setDescription('► Content: \n> ' + message.content)
+                .setFooter('Does anyone actually read this?')
+                .setTimestamp());
+            logger.messageDeleted(message, 'Hacker Report - Report processed');
+            message.delete();
+        }
+    }
+}
+
+module.exports.config = {
+    name: 'reporthackers',
+}
