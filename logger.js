0123aa2e441989e013dd61dd394b31de091cd0c6
JJ-G4M3R <josephzxhu@gmail.com>
2020-10-29 21:27:21 -0700

    Wording change

--- logger.js
+++ logger.js
@@ -3,7 +3,7 @@ const { MessageEmbed } = require("discord.js");
 module.exports = {
     messageDeleted(message, reason) {
         message.client.channels.cache.get('770758419737083944').send(new MessageEmbed()
-            .setTitle('Message Deleted by Bot')
+            .setTitle('Message Deleted')
             .setColor('RED')
             .setAuthor(`${message.author.tag} (${message.author.id})`, message.author.displayAvatarURL())
             .setDescription(`► Channel: **${message.channel.name}** \n► Message ID: ${message.id} \n► Content: \n> ${message.content}`)
