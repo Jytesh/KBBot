19c74b4202c2825bb4c072a464714858bae27c82
JJ-G4M3R <josephzxhu@gmail.com>
2020-11-27 18:44:01 -0800

    Updated guide link for submissions

--- commands/modmail.js
+++ commands/modmail.js
@@ -255,7 +255,7 @@ function denyRequest(member, user, reason, embed) {
         dm.send(new MessageEmbed()
             .setTitle('Submission request denied')
             .setColor('ORANGE')
-            .setDescription('Your submission request has been denied. For help, please check out the guide [here](https://discord.com/channels/448194623580667916/779620494328070144/779621435299725323). If you think this is a mistake, please contact ' + user.tag)
+            .setDescription('Your submission request has been denied. For help, please check out the guide [here](https://discord.com/channels/448194623580667916/779620494328070144/782073989681512449). If you think this is a mistake, please contact ' + user.tag)
             .addField('Reason:', reason)
             .setFooter('Submission denied by ' + member.username, member.displayAvatarURL())
             .setTimestamp());
