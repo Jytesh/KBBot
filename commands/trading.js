9fef6ca53d51288630b0b484c2cc5147ff2729f0
JJ-G4M3R <30411592+JJ-G4M3R@users.noreply.github.com>
2021-01-10 09:24:18 -0800

    Updated trading board

--- commands/trading.js
+++ commands/trading.js
@@ -12,9 +12,9 @@ const roles = [
 module.exports.run = (client, message) => {
     var canBypass = false;
     if (!canBypass) roles.forEach(role => { if (message.member.roles.cache.has(role)) canBypass = true; return });
-    if (!canBypass && message.content.indexOf('https://krunker.io/social.html?p=profile&q=') < 0 && message.content.indexOf('https://kr.social/p/') < 0 && message.attachments.size == 0) {
-        message.channel.send(`<@${message.author.id}> Your message was deleted for breaking the following rule(s): \n> All trades require an inventory screenshot with the username visible, or a link to your account. \n *This is an automated message.*`);
-        logger.messageDeleted(message, 'Trading board - Missing IGN/link', 'BLACK');
+    if (!canBypass && message.content.indexOf('https://krunker.io/social.html?p=profile&q=') < 0 && message.content.indexOf('https://kr.social/p/') < 0) {
+        message.channel.send(`<@${message.author.id}> Your message was deleted for breaking the following rule(s): \n> All trades require a link to your account. \n *This is an automated message.*`);
+        logger.messageDeleted(message, 'Trading board - Missing link', 'BLACK');
     }
 }
 
