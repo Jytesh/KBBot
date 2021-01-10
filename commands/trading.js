deff40f2d33d4a6cc60927601cff85aa1eed3b71
JJ-G4M3R <30411592+JJ-G4M3R@users.noreply.github.com>
2021-01-10 10:16:36 -0800

    trading board

--- commands/trading.js
+++ commands/trading.js
@@ -1,20 +1,29 @@
 const id = require('../id.json'),
-    logger = require('../logger');
+    logger = require('../logger'),
+    roles = [
+        id.roles.dev,
+        id.roles.yendis,
+        id.roles.cm,
+        id.roles.mod,
+        id.roles.tmod,
+    ],
+    socials = [
+        'https://krunker.io/social.html?p=profile&q=',
+        'https://kr.social/p/',
+    ];
 
-const roles = [
-    id.roles.dev,
-    id.roles.yendis,
-    id.roles.cm,
-    id.roles.mod,
-    id.roles.tmod,
-];
 
 module.exports.run = (client, message) => {
     var canBypass = false;
     if (!canBypass) roles.forEach(role => { if (message.member.roles.cache.has(role)) canBypass = true; return });
-    if (!canBypass && message.content.indexOf('https://krunker.io/social.html?p=profile&q=') < 0 && message.content.indexOf('https://kr.social/p/') < 0) {
-        message.channel.send(`<@${message.author.id}> Your message was deleted for breaking the following rule(s): \n> All trades require a link to your account. \n *This is an automated message.*`);
-        logger.messageDeleted(message, 'Trading board - Missing link', 'BLACK');
+    if (!canBypass) {
+        let rulesBroken = `<@${message.author.id}> Your message was deleted for breaking the following rule(s):`;
+        if (social.every(social => message.content.indexOf(social) < 0)) rulesBroken += `\n> All trades require a link to your account. Approved domains: \n> ► ${socials.join('\n> ► ')}`;
+        // if (link isnt from socials array) rulesBroken += '\n> No external links besides social hub links to your account.';
+        if (rulesBroken != `<@${message.author.id}> Your message was deleted for breaking the following rule(s):`) {
+            message.channel.send(rulesBroken + '\n *This is an automated message.*');
+            logger.messageDeleted(message, 'Trading board', 'BLACK');
+        }
     }
 }
 
