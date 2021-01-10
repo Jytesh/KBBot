9acac732caebfddbb0c5a4d46134e0e14c1aa198
JJ-G4M3R <30411592+JJ-G4M3R@users.noreply.github.com>
2021-01-10 10:32:31 -0800

    trading board

--- commands/trading.js
+++ commands/trading.js
@@ -1,16 +1,7 @@
 const id = require('../id.json'),
     logger = require('../logger'),
-    roles = [
-        id.roles.dev,
-        id.roles.yendis,
-        id.roles.cm,
-        id.roles.mod,
-        id.roles.tmod,
-    ],
-    socials = [
-        'https://krunker.io/social.html?p=profile&q=',
-        'https://kr.social/p/',
-    ];
+    roles = [id.roles.dev, id.roles.yendis, id.roles.cm, id.roles.mod, id.roles.tmod],
+    socials = ['https://krunker.io/social.html?p=profile&q=', 'https://kr.social/p/'];
 
 
 module.exports.run = (client, message) => {
@@ -18,7 +9,7 @@ module.exports.run = (client, message) => {
     if (!canBypass) roles.forEach(role => { if (message.member.roles.cache.has(role)) canBypass = true; return });
     if (!canBypass) {
         let rulesBroken = `<@${message.author.id}> Your message was deleted for breaking the following rule(s):`;
-        if (social.every(social => message.content.indexOf(social) < 0)) rulesBroken += `\n> All trades require a link to your account. Approved domains: \n> ► ${socials.join('\n> ► ')}`;
+        if (socials.every(social => message.content.indexOf(social) < 0)) rulesBroken += `\n> All trades require a link to your account. Approved domains: \n> ► ${socials.join('\n> ► ')}`;
         // if (link isnt from socials array) rulesBroken += '\n> No external links besides social hub links to your account.';
         if (rulesBroken != `<@${message.author.id}> Your message was deleted for breaking the following rule(s):`) {
             message.channel.send(rulesBroken + '\n *This is an automated message.*');
