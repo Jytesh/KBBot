e4937146ec904c7b72a72fe7cb479352fdb7ca08
JJ-G4M3R <30411592+JJ-G4M3R@users.noreply.github.com>
2021-01-11 14:03:42 -0800

    Added external link removal

--- commands/trading.js
+++ commands/trading.js
@@ -7,12 +7,12 @@ const id = require('../id.json'),
 module.exports.run = (client, message) => {
     var canBypass = false;
     if (!canBypass) roles.forEach(role => { if (message.member.roles.cache.has(role)) canBypass = true; return });
-    if (!canBypass) {
+    if (canBypass) {
         let rulesBroken = `<@${message.author.id}> Your message was deleted for breaking the following rule(s):`;
-        if (socials.every(social => message.content.indexOf(social) < 0)) rulesBroken += `\n> ► All trades require a link to your account. Approved domains: \n> ►► ${socials.join('\n> ►► ')}`;
-        //rulesBroken += '\n> ► No external links besides social hub links to your account.';
+        if (socials.every(social => message.content.indexOf(social) < 0)) rulesBroken += `\n> All trades require a link to your account. Approved domains: \n> ► ${socials.join('\n> ► ')}`;
+        if (message.content.replace(socials[0], '').replace(socials[1], '').includes('http')) rulesBroken += '\n> No external links besides social hub links to your account.';
         if (rulesBroken != `<@${message.author.id}> Your message was deleted for breaking the following rule(s):`) {
-            message.channel.send(rulesBroken + '\n *This is not an automated message.*').then(m => m.suppressEmbeds());
+            message.channel.send(rulesBroken + '\n *This is an automated message.*').then(m => m.suppressEmbeds());
             logger.messageDeleted(message, 'Trading board', 'BLACK');
         }
     }
